e0b40d0470fbe9bf4c0964b921fe34322f54a41a
lordoftheseas <73391479+lordoftheseas@users.noreply.github.com>
2024-03-23 22:48:34 -0400

    Necessary cleanup

--- components/Socials.js
+++ components/Socials.js
@@ -1,12 +1,6 @@
 import Link from "next/link";
 import { useState } from "react";
-import {
-  FaEnvelope,
-  FaEnvelopeOpen,
-  FaFileDownload,
-  FaPhone,
-  FaFilePdf,
-} from "react-icons/fa"; // Resume icon
+import { FaEnvelope, FaEnvelopeOpen, FaPhone, FaFilePdf } from "react-icons/fa"; // Resume icon
 import { RiLinkedinFill, RiInstagramFill, RiGithubFill } from "react-icons/ri";
 
 const Socials = () => {
@@ -79,71 +73,3 @@ const Socials = () => {
 };
 
 export default Socials;
-
-// // Import necessary modules
-// import Link from "next/link";
-// import { useState } from "react";
-// import {
-//   RiLinkedinFill,
-//   RiInstagramFill,
-//   RiFacebookFill,
-//   RiGithubFill,
-//   RiPhoneFill,
-// } from "react-icons/ri";
-
-// import { FaEnvelope, FaEnvelopeOpen, FaPhone } from "react-icons/fa";
-
-// const Socials = () => {
-//   // State to track hover status
-//   const [isHovered, setIsHovered] = useState(false);
-
-//   // Function to handle click and open email client
-//   const handleEnvelopeClick = () => {
-//     const email = "ramtelabhi04@gmail.com";
-//     window.location.href = `mailto:${email}`;
-//   };
-
-//   return (
-//     <div className="flex items-center gap-x-5 text-lg">
-//       <div
-//         onMouseEnter={() => setIsHovered(true)}
-//         onMouseLeave={() => setIsHovered(false)}
-//         onClick={handleEnvelopeClick}
-//         style={{
-//           cursor: "pointer",
-//         }}
-//         className={isHovered ? "text-accent" : ""}
-//       >
-//         {isHovered ? <FaEnvelopeOpen /> : <FaEnvelope />}
-//       </div>
-//       {/* <Link
-//         href="tel:+1234567890"
-//         className="hover:text-accent transition-all duration-300"
-//         passHref
-//       >
-//         <FaPhone />
-//       </Link> */}
-
-//       <Link href={""} className="hover:text-accent transition-all duration-300">
-//         <RiLinkedinFill />
-//       </Link>
-//       <Link href={""} className="hover:text-accent transition-all duration-300">
-//         <RiInstagramFill />
-//       </Link>
-//       <Link href={""} className="hover:text-accent transition-all duration-300">
-//         <RiFacebookFill />
-//       </Link>
-//       <Link href={""} className="hover:text-accent transition-all duration-300">
-//         <RiGithubFill />
-//       </Link>
-// <Link
-//   href={"tel:+1234567890"}
-//   className="hover:text-accent transition-all duration-300"
-// >
-//   <RiPhoneFill />
-// </Link>
-//     </div>
-//   );
-// };
-
-// export default Socials;
--- pages/about/index.js
+++ pages/about/index.js
@@ -30,6 +30,12 @@ import {
 } from "react-icons/si";
 
 const About = () => {
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <motion.div
       className="h-full overflow-y-auto"
@@ -49,132 +55,159 @@ const About = () => {
               height={500}
             />
           </div>
-          <div className="flex flex-col gap-12 justify-center">
-            <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
-            <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
-            {/* BIOGRAPHY QUOTE  */}
-            <span className="italic">
-              A handcrafted, small-batch, artisinal pour-over version of the
-              classic lorem ipsum generato.
-            </span>
-            {/* OUR SVG SIGNATURE BELOW */}
-            <div className="self-end"></div>
-            <div className="self-end">{/* SVG content */}</div>
-          </div>
-
-          <div className="flex flex-col gap-12 justify-center">
-            <h1 className="font-bold text-2xl">SKILLS</h1>
-            {/* SKILLS LIST */}
-            <div className="flex gap-4 flex-wrap">
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Python
-                <FaPython className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Java
-                <FaJava className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                JavaScript
-                <FaJs className="ml-2" />
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                C/C++
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                HTML5
-                <FaHtml5 className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                CSS3
-                <FaCss3 className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                MySQL
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Arduino C
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                React Native
-                <FaReact className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Git
-                <FaGit className="ml-2" />
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                React.js
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Node.js
-                <FaNodeJs className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Next.js
-                <SiNextdotjs className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Figma
-                <FaFigma className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Linux
-                <FaLinux className="ml-2" />
-              </div>
-              <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                GitHub
-                <FaGithub className="ml-2" />
-              </div>
+          <div>
+            <div
+              className="flex flex-col gap-14 justify-center h-full"
+              id="biography-section"
+            >
+              <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
+              <p className="text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+              </p>
+              {/* BIOGRAPHY QUOTE  */}
+              <span className="italic">
+                A handcrafted, small-batch, artisinal pour-over version of the
+                classic lorem ipsum generato.
+              </span>
+              {/* OUR SVG SIGNATURE BELOW */}
+              {/* <div className="self-end"></div> */}
+              {/* <div className="self-end">SVG content</div> */}
+              <button
+                className="text-4xl text-gray-500 cursor-pointer"
+                onClick={() => handleScroll("skills-section")}
+              >
+                <FaArrowDown />
+              </button>
             </div>
           </div>
-
-          <div className="flex flex-col gap-12 justify-center pb-48">
-            <h1 className="font-bold text-2xl">EXPERIENCE</h1>
-            <div className="">
-              {/* Map over experienceData to dynamically generate experience sections */}
-              {experienceData.map((experience, index) => (
-                <div
-                  key={index}
-                  className={`flex justify-between h-auto ${
-                    index % 2 === 0 ? "flex-row-reverse" : ""
-                  }`}
-                >
-                  <div className="w-3/4"></div>
-                  <div className="w-1/6 flex items-center justify-center">
-                    <div className="w-1 h-full bg-gray-600 rounded relative">
-                      <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
-                    </div>
-                  </div>
-                  <div className="w-3/4">
-                    {/* this is where the gridbox changes its shape */}
-                    <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg text-black">
-                      {experience.jobTitle}
-                    </div>
-                    <ul className="p-3 text-sm list-disc italic">
-                      {/* Use map to render each responsibility as a bullet point */}
-                      {experience.responsibilities.map(
-                        (responsibility, idx) => (
-                          <li key={idx}>{responsibility}</li>
-                        )
-                      )}
-                    </ul>
-                    <div className="p-3 text-red-400 text-sm font-semibold">
-                      {experience.date}
-                    </div>
-                    <div className="p-1 rounded bg-white text-sm font-semibold text-black w-fit">
-                      {experience.company}
-                    </div>
-                  </div>
+          {/* NEW SECTION */}
+          <div>
+            <div
+              className="flex flex-col gap-12 justify-center h-screen"
+              id="skills-section"
+            >
+              <h1 className="font-bold text-2xl">SKILLS</h1>
+              {/* SKILLS LIST */}
+              <div className="flex gap-4 flex-wrap">
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Python
+                  <FaPython className="ml-2" />
                 </div>
-              ))}
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Java
+                  <FaJava className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  JavaScript
+                  <FaJs className="ml-2" />
+                </div>
+                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  C/C++
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  HTML5
+                  <FaHtml5 className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  CSS3
+                  <FaCss3 className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  MySQL
+                </div>
+                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Arduino C
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  React Native
+                  <FaReact className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Git
+                  <FaGit className="ml-2" />
+                </div>
+                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  React.js
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Node.js
+                  <FaNodeJs className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Next.js
+                  <SiNextdotjs className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Figma
+                  <FaFigma className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  Linux
+                  <FaLinux className="ml-2" />
+                </div>
+                <div className="flex items-center rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                  GitHub
+                  <FaGithub className="ml-2" />
+                </div>
+              </div>
+              <button
+                className="text-4xl text-gray-500 cursor-pointer"
+                onClick={() => handleScroll("experience-section")}
+              >
+                <FaArrowDown />
+              </button>
+            </div>
+          </div>
+          {/* NEW SECTION */}
+          <div>
+            <div
+              className="flex flex-col gap-12 justify-center pb-48 h-full"
+              id="experience-section"
+            >
+              <h1 className="font-bold text-2xl">EXPERIENCE</h1>
+              <div className="">
+                {/* Map over experienceData to dynamically generate experience sections */}
+                {experienceData.map((experience, index) => (
+                  <div
+                    key={index}
+                    className={`flex justify-between h-auto ${
+                      index % 2 === 0 ? "flex-row-reverse" : ""
+                    }`}
+                  >
+                    <div className="w-3/4"></div>
+                    <div className="w-1/6 flex items-center justify-center">
+                      <div className="w-1 h-full bg-gray-600 rounded relative">
+                        <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                      </div>
+                    </div>
+                    <div className="w-3/4">
+                      {/* this is where the gridbox changes its shape */}
+                      <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg text-black">
+                        {experience.jobTitle}
+                      </div>
+                      <ul className="p-3 text-sm list-disc italic">
+                        {/* Use map to render each responsibility as a bullet point */}
+                        {experience.responsibilities.map(
+                          (responsibility, idx) => (
+                            <li key={idx}>{responsibility}</li>
+                          )
+                        )}
+                      </ul>
+                      <div className="p-3 text-red-400 text-sm font-semibold">
+                        {experience.date}
+                      </div>
+                      <div className="p-1 rounded bg-white text-sm font-semibold text-black w-fit">
+                        {experience.company}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
